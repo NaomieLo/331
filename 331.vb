a0196a5700ff90293f394c2efd4ed0261aa378fa
NaomieLo <68392993+NaomieLo@users.noreply.github.com>
2024-02-19 08:44:44 -0500

    Create 331.vb

---
+++ 331.vb
@@ -0,0 +1,364 @@
+Sub UnHideQuestions()
+'
+' UnHideQuestions Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+End Sub
+Sub BacktoDashboard()
+'
+' UnHideQuestions Macro
+'
+
+'
+    Sheets("Dashboard").Select
+    Rows("2:100").EntireRow.Hidden = False
+End Sub
+
+Sub Question1()
+'
+' Question1 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("3:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question2()
+'
+' Question2 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:2").EntireRow.Hidden = True
+    Rows("4:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question3()
+'
+' Question3 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:3").EntireRow.Hidden = True
+    Rows("5:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question4()
+'
+' Question4 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:4").EntireRow.Hidden = True
+    Rows("6:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question5()
+'
+' Question5 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:5").EntireRow.Hidden = True
+    Rows("7:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question6()
+'
+' Question6 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:6").EntireRow.Hidden = True
+    Rows("8:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question7()
+'
+' Question7 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:7").EntireRow.Hidden = True
+    Rows("9:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question8()
+'
+' Question8 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:8").EntireRow.Hidden = True
+    Rows("10:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question9()
+'
+' Question9 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:9").EntireRow.Hidden = True
+    Rows("11:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question10()
+'
+' Question10 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:10").EntireRow.Hidden = True
+    Rows("12:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question11()
+'
+' Question11 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:11").EntireRow.Hidden = True
+    Rows("13:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question12()
+'
+' Question12 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:12").EntireRow.Hidden = True
+    Rows("14:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question13()
+'
+' Question13 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:13").EntireRow.Hidden = True
+    Rows("15:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question14()
+'
+' Question14 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:14").EntireRow.Hidden = True
+    Rows("16:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question15()
+'
+' Question15 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:15").EntireRow.Hidden = True
+    Rows("17:100").EntireRow.Hidden = True
+End Sub
+
+Sub Question16()
+'
+' Question16 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:16").EntireRow.Hidden = True
+    Rows("18:100").EntireRow.Hidden = True
+End Sub
+Sub Question17()
+'
+' Question17 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:17").EntireRow.Hidden = True
+    Rows("19:100").EntireRow.Hidden = True
+End Sub
+Sub Question18()
+'
+' Question18 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:18").EntireRow.Hidden = True
+    Rows("26:100").EntireRow.Hidden = True
+End Sub
+Sub Question19()
+'
+' Question19 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:25").EntireRow.Hidden = True
+    Rows("27:100").EntireRow.Hidden = True
+End Sub
+Sub Question20()
+'
+' Question20 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:26").EntireRow.Hidden = True
+    Rows("39:100").EntireRow.Hidden = True
+End Sub
+Sub Question21()
+'
+' Question21 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:38").EntireRow.Hidden = True
+    Rows("48:100").EntireRow.Hidden = True
+End Sub
+Sub Question22()
+'
+' Question22 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:47").EntireRow.Hidden = True
+    Rows("49:100").EntireRow.Hidden = True
+End Sub
+Sub Question23()
+'
+' Question22 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:48").EntireRow.Hidden = True
+    Rows("50:100").EntireRow.Hidden = True
+End Sub
+Sub Question24()
+'
+' Question24 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:49").EntireRow.Hidden = True
+    Rows("56:100").EntireRow.Hidden = True
+End Sub
+Sub Question25()
+'
+' Question25 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:55").EntireRow.Hidden = True
+    Rows("70:100").EntireRow.Hidden = True
+End Sub
+Sub Question26()
+'
+' Question26 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:70").EntireRow.Hidden = True
+    Rows("77:100").EntireRow.Hidden = True
+End Sub
+Sub Question27()
+'
+' Question27 Macro
+'
+
+'
+    Sheets("Questions").Select
+    Rows("2:100").EntireRow.Hidden = False
+    Rows("2:77").EntireRow.Hidden = True
+
+End Sub
+
+Sub CreateQuestionButtons()
+    Dim ws As Worksheet
+    Dim i As Integer
+
+    ' Set the worksheet
+    Set ws = ThisWorkbook.Sheets("Dashboard")
+
+    ' Loop to create buttons and assign macros
+    For i = 19 To 27
+        With ws.Buttons.Add(50, 20 * i, 80, 20)
+            .Caption = "Question " & i
+            
+        End With
+    Next i
+End Sub
+
+Sub ToggleQuestion(ByVal questionNum As Integer)
+    Dim ws As Worksheet
+    Dim hideStartRow As Integer
+
+    ' Set the worksheet
+    Set ws = ThisWorkbook.Sheets("Questions")
+
+    ' Determine the starting row to hide
+    hideStartRow = 1 + questionNum
+
+    ' Toggle visibility of rows
+    With ws.Rows(hideStartRow & ":100")
+        .EntireRow.Hidden = Not .EntireRow.Hidden
+    End With
+End Sub
+
+
